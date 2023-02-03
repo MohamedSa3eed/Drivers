11fa75a438bb775b57e09fe50031172e12993fec
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-02-03 22:12:14 +0200

    creat STD_TYPES.h

---
+++ STM32F103xx/LIB/STD_TYPES/STD_TYPES.h
@@ -0,0 +1,15 @@
+/************************************************************************/
+/* Autthor      :  Mohamed Saeed                                        */
+/* E-Mail       :  mohamed.sa3ed.faried@gmail.com                       */
+/* Date         :  3 FEB 2023                                           */
+/* Version      :  V01                                                  */
+/************************************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char          u8  ;
+typedef unsigned short int     u16 ;
+typedef unsigned long int      u32 ;
+
+
+#endif // !STD_TYPES_H
