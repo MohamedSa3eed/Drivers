6a0b6af9a7c2ee6cb3c5d3e1c730bcb8ba2a40ca
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-02-09 15:55:29 +0200

    add Pins functions to GPIO

--- STM32F103xx/MCAL/GPIO/GPIO_interface.h
+++ STM32F103xx/MCAL/GPIO/GPIO_interface.h
@@ -29,6 +29,28 @@
 #define   PIN14       14  
 #define   PIN15       15  
 
+//Modes
+#define     GPIO_OUT_10M_PP       0b0001
+#define     GPIO_OUT_2M_PP        0b0010
+#define     GPIO_OUT_50M_PP       0b0011
+#define     GPIO_OUT_10M_OD       0b0101
+#define     GPIO_OUT_2M_OD        0b0110
+#define     GPIO_OUT_50M_OD       0b0111
+#define     GPIO_OUT_10M_AFPP     0b1001
+#define     GPIO_OUT_2M_AFPP      0b1010
+#define     GPIO_OUT_50M_AFPP     0b1011
+#define     GPIO_OUT_10M_AFOD     0b1101
+#define     GPIO_OUT_2M_AFOD      0b1110
+#define     GPIO_OUT_50M_AFOD     0b1111
+#define     GPIO_IN_ANALOG        0b0000
+#define     GPIO_IN_FLOATING      0b0100
+#define     GPIO_IN_PULL_UP_DOWN  0b1000
+
+//Directions 
+#define     GPIO_HIGH             1 
+#define     GPIO_LOW              0 
+
+
 void GPIO_voidSetPinDirection(u8 copy_u8Port , u8 copy_u8Pin , u8 copy_u8Mode);
 void GPIO_voidSetPinValue(u8 copy_u8Port , u8 copy_u8Pin , u8 copy_u8Value);
 u8   GPIO_u8GetPinValue(u8 copy_u8Port , u8 copy_u8Pin);
--- STM32F103xx/MCAL/GPIO/GPIO_program.c
+++ STM32F103xx/MCAL/GPIO/GPIO_program.c
@@ -19,11 +19,129 @@ void GPIO_voidSetPinDirection(u8 copy_u8Port, u8 copy_u8Pin, u8 copy_u8Mode)
     case GPIOA :
                   if (copy_u8Pin < 8 )
                   {
-                      
-
+                      GPIOA_CRL &= ~ ((0b1111) << (copy_u8Pin *4 ));
+                      GPIOA_CRL |= ~ ((copy_u8Mode) << (copy_u8Pin *4 ));
                   }
+                  else if (copy_u8Pin <16) 
+                  {
+                      copy_u8Pin -= 8 ;
+                      GPIOA_CRH &= ~ ((0b1111) << (copy_u8Pin *4 ));
+                      GPIOA_CRH |= ~ ((copy_u8Mode) << (copy_u8Pin *4 ));
+                  }
+                  else 
+                  {
+                      #error ("Wrong Pin\n")
+                  }
+                  break;
     case GPIOB :
+                  if (copy_u8Pin < 8 )
+                  {
+                      GPIOB_CRL &= ~ ((0b1111) << (copy_u8Pin *4 ));
+                      GPIOB_CRL |= ~ ((copy_u8Mode) << (copy_u8Pin *4 ));
+                  }
+                  else if (copy_u8Pin <16) 
+                  {
+                      copy_u8Pin -= 8 ;
+                      GPIOB_CRH &= ~ ((0b1111) << (copy_u8Pin *4 ));
+                      GPIOB_CRH |= ~ ((copy_u8Mode) << (copy_u8Pin *4 ));
+                  }
+                  else 
+                  {
+                      #error ("Wrong Pin\n")
+                  }
+                  break;
     case GPIOC :
+                  if (copy_u8Pin < 8 )
+                  {
+                      GPIOC_CRL &= ~ ((0b1111) << (copy_u8Pin *4 ));
+                      GPIOC_CRL |= ~ ((copy_u8Mode) << (copy_u8Pin *4 ));
+                  }
+                  else if (copy_u8Pin <16) 
+                  {
+                      copy_u8Pin -= 8 ;
+                      GPIOC_CRH &= ~ ((0b1111) << (copy_u8Pin *4 ));
+                      GPIOC_CRH |= ~ ((copy_u8Mode) << (copy_u8Pin *4 ));
+                  }
+                  else 
+                  {
+                      #error ("Wrong Pin\n")
+                  }
+                  break;
+    default    : 
+                  #error ("Wrong Port\n")
+                  break;
   }
+}
 
+void GPIO_voidSetPinValue(u8 copy_u8Port, u8 copy_u8Pin, u8 copy_u8Value)
+{
+  switch (copy_u8Port) 
+  {
+    case GPIOA :
+                  if (copy_u8Value == GPIO_HIGH)
+                  {
+                      SET_BIT(GPIOA_ODR, copy_u8Pin);
+                  }
+                  else if (copy_u8Value == GPIO_LOW) 
+                  {
+                      CLR_BIT(GPIOA_ODR, copy_u8Pin);
+                  }
+                  else 
+                  {
+                      #error ("Wrong Value\n")
+                  }
+                  break;
+    case GPIOB :
+                  if (copy_u8Value == GPIO_HIGH)
+                  {
+                      SET_BIT(GPIOB_ODR, copy_u8Pin);
+                  }
+                  else if (copy_u8Value == GPIO_LOW) 
+                  {
+                      CLR_BIT(GPIOB_ODR, copy_u8Pin);
+                  }
+                  else 
+                  {
+                      #error ("Wrong Value\n")
+                  }
+                  break;
+    case GPIOC :
+                  if (copy_u8Value == GPIO_HIGH)
+                  {
+                      SET_BIT(GPIOC_ODR, copy_u8Pin);
+                  }
+                  else if (copy_u8Value == GPIO_LOW) 
+                  {
+                      CLR_BIT(GPIOC_ODR, copy_u8Pin);
+                  }
+                  else 
+                  {
+                      #error ("Wrong Value\n")
+                  }
+                  break;
+    default    : 
+                  #error ("Wrong Port\n")
+                  break;
+  }
+}
+
+u8   GPIO_u8GetPinValue(u8 copy_u8Port , u8 copy_u8Pin)
+{
+  u8 local_u8Value = 0;
+  switch (copy_u8Port) 
+  {
+    case GPIOA :
+                  local_u8Value = GET_BIT(GPIOA_IDR , copy_u8Pin);
+                  break;
+    case GPIOB :
+                  local_u8Value = GET_BIT(GPIOB_IDR , copy_u8Pin);
+                  break;
+    case GPIOC :
+                  local_u8Value = GET_BIT(GPIOC_IDR , copy_u8Pin);
+                  break;
+    default    : 
+                  #error ("Wrong Port\n")
+                  break;
+  }
+    return local_u8Value;
 }
