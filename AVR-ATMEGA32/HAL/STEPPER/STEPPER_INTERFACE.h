2f82f69b1e7bafd330865a1193d1774adb41efe1
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-08-14 19:18:04 +0300

    handeled error state in Stepper motor driver for atmega32

--- AVR-ATMEGA32/HAL/STEPPER/STEPPER_INTERFACE.h
+++ AVR-ATMEGA32/HAL/STEPPER/STEPPER_INTERFACE.h
@@ -2,13 +2,13 @@
 #define STEPPER_INTERFACE_H
 
 /* this fuction initialize the pins connected to stepper motor */
-void STEPPER_Init();
+ES_t STEPPER_Init(void);
 
 /*  this functoin makes the stepper do the primary steps */
-void STEPPER_FullStep(void);
+ES_t STEPPER_FullStep(void);
 
 
 /*  this functoin makes the stepper do the primary & secondary steps */
-void STEPPER_HalfStep(void);
+ES_t STEPPER_HalfStep(void);
 
 #endif
--- AVR-ATMEGA32/HAL/STEPPER/STEPPER_PROGRAM.c
+++ AVR-ATMEGA32/HAL/STEPPER/STEPPER_PROGRAM.c
@@ -1,125 +1,276 @@
 #include "../../LIB/STD_TYPES.h"
+#include "../../LIB/ERR_STATE.h"
 #include "../../MCAL/DIO/DIO_INTERFACE.h"
 #include "STEPPER_INTERFACE.h"
 #include "STEPPER_CONFIG.h"
 #include </usr/avr/include/avr/delay.h>
 
-void STEPPER_Init()
+ES_t STEPPER_Init(void)
 {
-	DIO_SetPinDirection(STEPPER_BLUE_PIN,OUTPUT);
-	DIO_SetPinDirection(STEPPER_PINK_PIN,OUTPUT);
-	DIO_SetPinDirection(STEPPER_YELLOW_PIN,OUTPUT);
-	DIO_SetPinDirection(STEPPER_ORANGE_PIN,OUTPUT);
+  ES_t Local_u8ErrorState = ES_NOK ;
+  Local_u8ErrorState =	DIO_SetPinDirection(STEPPER_BLUE_PIN,OUTPUT);
+  if (Local_u8ErrorState != ES_OK) {
+    return Local_u8ErrorState ;
+  }
+  Local_u8ErrorState =	DIO_SetPinDirection(STEPPER_PINK_PIN,OUTPUT);
+  if (Local_u8ErrorState != ES_OK) {
+    return Local_u8ErrorState ;
+  }
+  Local_u8ErrorState =	DIO_SetPinDirection(STEPPER_YELLOW_PIN,OUTPUT);
+  if (Local_u8ErrorState != ES_OK) {
+    return Local_u8ErrorState ;
+  }
+  Local_u8ErrorState =	DIO_SetPinDirection(STEPPER_ORANGE_PIN,OUTPUT);
+  return Local_u8ErrorState ;
 }
 
-void STEPPER_FullStep()
+ES_t STEPPER_FullStep(void)
 {
-	u8 Locale_value = DIO_GetPortValue(STEPPER_PORT);
-	switch (Locale_value) {
+  ES_t Local_u8ErrorState = ES_NOK ;
+	u8 Local_pu8Value ;
+  Local_u8ErrorState = DIO_GetPortValue(STEPPER_PORT , &Local_pu8Value);
+  if (Local_u8ErrorState != ES_OK) {
+    return Local_u8ErrorState ;
+  }
+	switch (Local_pu8Value) {
 		case 0b00000000 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
-                case 0b00000001:
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+    case 0b00000001:
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
-                        break;
+      break;
 		case 0b00000010:
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
-                case 0b00000100 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
+    case 0b00000100 :
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
 			_delay_ms(5);
 			break;
 		case 0b00001000 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
-
 	}
+  return Local_u8ErrorState ;
 }
-void STEPPER_HalfStep()
+
+ES_t STEPPER_HalfStvoidep(void)
 {
-	u8 Locale_value = DIO_GetPortValue(STEPPER_PORT);
-        switch (Locale_value) {
+  ES_t Local_u8ErrorState = ES_NOK ;
+  u8 Local_pu8Value ;
+  Local_u8ErrorState = DIO_GetPortValue(STEPPER_PORT , &Local_pu8Value);
+  if (Local_u8ErrorState != ES_OK) {
+    return Local_u8ErrorState ;
+  }
+  switch (Local_pu8Value) 
+  {
 		case 0b00000000 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =	DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
 			_delay_ms(5);
 			break;
-                case 0b00001001:
-			DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+    case 0b00001001:
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
-                        break;
+      break;
 		case 0b00000001:
-			DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
-                case 0b00000011 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+    case 0b00000011 :
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
 		case 0b00000010 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
-                case 0b00000110 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
+    case 0b00000110 :
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, LOW);
 			_delay_ms(5);
 			break;
-                case 0b00000100 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
+    case 0b00000100 :
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
 			_delay_ms(5);
 			break;
-                case 0b00001100 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
+    case 0b00001100 :
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
 			_delay_ms(5);
 			break;
-                case 0b00001000 :
-			DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
-			DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
-			DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
-			DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
+    case 0b00001000 :
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_BLUE_PIN, HIGH);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_PINK_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_YELLOW_PIN, LOW);
+      if (Local_u8ErrorState != ES_OK) {
+        return Local_u8ErrorState ;
+      }
+      Local_u8ErrorState =  DIO_SetPinValue(STEPPER_ORANGE_PIN, HIGH);
 			_delay_ms(5);
 			break;
 	}
+  return Local_u8ErrorState ;
 }
