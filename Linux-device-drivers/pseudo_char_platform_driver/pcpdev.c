6806876b9fcc6ddcb91cccddbb6476f7a3075d33
MohamedSa3eed <mohamed.sa3ed.faried@gmail.com>
2023-12-22 19:41:35 +0200

    implemented probe and remove function

--- Linux-device-drivers/pseudo_char_platform_driver/pcpdev.c
+++ Linux-device-drivers/pseudo_char_platform_driver/pcpdev.c
@@ -1,6 +1,7 @@
-#include <linux/module.h>
+#include "platform_data.h"
 #include <linux/cdev.h>
 #include <linux/fs.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
 
 MODULE_LICENSE("GPL");
@@ -8,32 +9,60 @@ MODULE_AUTHOR("Sa3ed");
 MODULE_DESCRIPTION("A simple pseudo platform device");
 
 static void pcpdev_release(struct device *dev) {
-  printk(KERN_INFO "%s: device %s released\n",__func__, dev_name(dev));
+  printk(KERN_INFO "%s: device %s released\n", __func__, dev_name(dev));
 }
 
-struct platform_device pdev = {
+struct platform_data pdata[2] = {
+    [0] = {
+            .memorySize = 1024,
+            .serial_number = "pcp-1234",
+          },
+    [1] = {
+            .memorySize = 512,
+            .serial_number = "pcp-5678",
+          }
+};
+
+struct platform_device pdev0 = {
+    .name = "pcp-drv",
+    .id = 0,
+    .dev =
+        {
+            .platform_data = &pdata[0],
+            .release = pcpdev_release,
+        },
+};
+
+struct platform_device pdev1 = {
     .name = "pcp-drv",
     .id = 1,
-    .dev = {
-        .platform_data = "This is platform data",
-        .release = pcpdev_release,
-    },
+    .dev =
+        {
+            .platform_data = &pdata[1],
+            .release = pcpdev_release,
+        },
 };
 
 static int __init pcpdev_init(void) {
   int ret;
-  ret = platform_device_register(&pdev);
+  ret = platform_device_register(&pdev0);
   if (ret) {
-    printk(KERN_ERR "%s: failed to load platform device\n",__func__);
+    printk(KERN_ERR "%s: failed to load platform device\n", __func__);
     return ret;
   }
-  printk(KERN_INFO "%s: platform device loaded\n",__func__);
+  ret = platform_device_register(&pdev1);
+  if (ret) {
+    printk(KERN_ERR "%s: failed to load platform device\n", __func__);
+    return ret;
+  }
+  printk(KERN_INFO "%s: platform device loaded\n", __func__);
   return 0;
 }
 
 static void __exit pcpdev_exit(void) {
-  platform_device_unregister(&pdev);
-  printk(KERN_INFO "%s: platform device unloaded\n",__func__);
+  platform_device_unregister(&pdev0);
+  platform_device_unregister(&pdev1);
+  printk(KERN_INFO "%s: platform device unloaded\n", __func__);
 }
 
 module_init(pcpdev_init);
--- Linux-device-drivers/pseudo_char_platform_driver/pcpdrv.c
+++ Linux-device-drivers/pseudo_char_platform_driver/pcpdrv.c
@@ -1,40 +1,32 @@
 #include "linux/kern_levels.h"
-#include <linux/module.h>
+#include "platform_data.h"
 #include <linux/cdev.h>
+#include <linux/device.h>
 #include <linux/fs.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sa3ed");
 MODULE_DESCRIPTION("A simple pseudo platform driver");
 
+#define MAX_DEVICES 10
+
+struct platform_device_data {
+  dev_t devt;                  // device number
+  char *buffer;                // buffer to store data
+  struct cdev cdev;            // character device
+  struct platform_data *pdata; // platform data
+};
+
 struct platform_driver_data {
-  int total_devices;
-  dev_t devt;
-  struct class *class;
+  int total_devices;   // total number of devices
+  dev_t devt;          // device base number
+  struct class *class; // device class
+  struct device *dev;  // device
 };
 
-// get called when a device is detected
-static int pcpdrv_probe(struct platform_device *pdev) {
-  printk(KERN_INFO "%s: platform device detected\n", __func__);
-  return 0;
-}
-
-// get called when a device is removed
-static int pcpdrv_remove(struct platform_device *pdev) {
-  printk(KERN_INFO "%s: platform device removed\n", __func__);
-  return 0;
-}
-
-static struct platform_driver pcp_driver = {
-    .probe = pcpdrv_probe,
-    .remove = pcpdrv_remove,
-    .driver =
-        {
-            .name = "pcp-drv",
-            .owner = THIS_MODULE,
-        },
-};
+struct platform_driver_data pcpdrv_data;
 
 static int pcpdrv_open(struct inode *inode, struct file *file) {
   printk(KERN_INFO "pcp_open: called\n");
@@ -72,15 +64,124 @@ struct file_operations pcpdrv_fops = {
     .llseek = pcpdrv_llseek,
 };
 
+// get called when a device is detected
+static int pcpdrv_probe(struct platform_device *pdev) {
+  struct platform_device_data *pdev_data;
+  struct platform_data *pdata;
+  int ret;
+
+  // get the platform data
+  pdata = (struct platform_data *)dev_get_platdata(&pdev->dev);
+  if (!pdata) {
+    printk(KERN_ERR "%s: failed to get platform data\n", __func__);
+    return -EINVAL;
+  }
+
+  // allocate memory for the device
+  pdev_data = (struct platform_device_data *)devm_kzalloc(
+      &pdev->dev, sizeof(struct platform_device_data), GFP_KERNEL);
+  if (!pdev_data) {
+    printk(KERN_ERR "%s: failed to allocate memory for device\n", __func__);
+    return -ENOMEM;
+  }
+  pdev_data->pdata = (struct platform_data *)devm_kzalloc(
+      &pdev->dev, sizeof(struct platform_data), GFP_KERNEL);
+  if (!pdev_data->pdata) {
+    printk(KERN_ERR "%s: failed to allocate memory for pdata\n", __func__);
+    return -ENOMEM;
+  }
+  pdev_data->pdata->memorySize = pdata->memorySize;
+  pdev_data->pdata->serial_number = pdata->serial_number;
+  pdev_data->buffer =
+      (char *)devm_kzalloc(&pdev->dev, pdata->memorySize, GFP_KERNEL);
+  if (!pdev_data->buffer) {
+    printk(KERN_ERR "%s: failed to allocate memory for device data\n", __func__);
+    return -ENOMEM;
+  }
+
+  // set platform device data
+  /*pdev->dev.driver_data = pdev_data;*/
+  dev_set_drvdata(&pdev->dev, pdev_data);
+
+  // get device number
+  pdev_data->devt = pcpdrv_data.devt + pdev->id;
+
+  // initialize character device
+  cdev_init(&pdev_data->cdev, &pcpdrv_fops);
+  ret = cdev_add(&pdev_data->cdev, pdev_data->devt, 1);
+  if (ret < 0) {
+    printk(KERN_ERR "%s: failed to add device\n", __func__);
+    return ret;
+  }
+
+  // create device file
+  pcpdrv_data.dev = device_create(pcpdrv_data.class, &pdev->dev,
+                                  pdev_data->devt, NULL, "pcp-dev%d", pdev->id);
+  if (IS_ERR(pcpdrv_data.dev)) { // Check if device creation failed
+    printk(KERN_ERR "%s: Device file creation failed\n", __func__);
+    ret = PTR_ERR(pcpdrv_data.dev);
+    return ret;
+  }
+
+  pcpdrv_data.total_devices++; // increment total devices
+
+  printk(KERN_INFO "%s: platform device detected\n", __func__);
+  printk(KERN_INFO "%s: device_name: %s\n", __func__, pdev->name);
+  printk(KERN_INFO "%s: memory size: %d\n", __func__, pdata->memorySize);
+  printk(KERN_INFO "%s: serial number: %s\n", __func__, pdata->serial_number);
+  return 0;
+}
+
+// get called when a device is removed
+static int pcpdrv_remove(struct platform_device *pdev) {
+  struct platform_device_data *pdev_data;
+  pdev_data = dev_get_drvdata(&pdev->dev); // get platform device data
+  device_destroy(pcpdrv_data.class, pdev_data->devt); // destroy device file
+  cdev_del(&pdev_data->cdev);                         // delete character device
+  pcpdrv_data.total_devices--;                        // decrement total devices
+
+  printk(KERN_INFO "%s: platform device removed\n", __func__);
+  return 0;
+}
+
+static struct platform_driver pcp_driver = {
+    .probe = pcpdrv_probe,
+    .remove = pcpdrv_remove,
+    .driver =
+        {
+            .name = "pcp-drv",
+            .owner = THIS_MODULE,
+        },
+};
+
 int __init pcpdrv_init(void) {
+  int ret;
+  ret = alloc_chrdev_region(&pcpdrv_data.devt, 0, MAX_DEVICES, "pcp-dev");
+  if (ret < 0) {
+    printk(KERN_ERR "%s: failed to allocate chrdev region\n", __func__);
+    return ret;
+  }
+  pcpdrv_data.class = class_create(THIS_MODULE, "pcp-class");
+  if (IS_ERR(pcpdrv_data.class)) {
+    printk(KERN_ERR "%s: failed to create class\n", __func__);
+    unregister_chrdev_region(pcpdrv_data.devt, MAX_DEVICES);
+    ret = PTR_ERR(pcpdrv_data.class);
+    return ret;
+  }
+  ret = platform_driver_register(&pcp_driver);
+  if (ret < 0) {
+    printk(KERN_ERR "%s: failed to register platform driver\n", __func__);
+    return ret;
+  }
   printk(KERN_INFO "%s: platform driver registered\n", __func__);
-  return platform_driver_register(&pcp_driver);
   return 0;
 }
 
 void __exit pcpdrv_exit(void) {
-  printk(KERN_INFO "%s: platform driver unregistered\n", __func__);
   platform_driver_unregister(&pcp_driver);
+  class_destroy(pcpdrv_data.class);
+  unregister_chrdev_region(pcpdrv_data.devt, MAX_DEVICES);
+  printk(KERN_INFO "%s: platform driver unregistered\n", __func__);
 }
 
 module_init(pcpdrv_init);
